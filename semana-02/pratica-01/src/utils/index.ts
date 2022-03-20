9918e144b6b1f8cf9cadd03398d1fd1e4c25ef2d
Carlos Marinho <carlos.marinho20@outlook.com>
2022-03-20 02:23:08 -0300

    Delete semana-02, semana-03 and semana-04/pratica-react-02 directories

--- semana-02/pratica-01/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const getNotEquals = <T extends Record<string, unknown>>(
-  itemA: T,
-  itemB: T,
-  keys: (keyof T)[]
-): keyof T | undefined => {
-  return keys.find((key) => itemA[key] !== itemB[key]);
-};
